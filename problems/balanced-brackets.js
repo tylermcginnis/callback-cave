6ef3ca927021e391f1d3845447badfbfa8d9718b
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-10-08 17:24:32 -0600

    Add problem

---
+++ problems/balanced-brackets.js
@@ -0,0 +1,8 @@
+/*Write a function that accepts a string consisting entiring of brackets ([](){}) 
+and returns whether it is balanced. Every "opening" bracket must be followed by a closing 
+bracket of the same type. There can also be nested brackets, which adhere to the same rule.*/
+/*
+f('()[]{}(([])){[()][]}') // true
+f('())[]{}') // false
+f('[(])') // false
+*/
